03f703c87a81a5f39470a1cadcda9add6b93fc53
purr <x@x.x>
2025-02-05 22:52:55 +0100

    Refactor config.h with static device variables

--- src/config.h
+++ src/config.h
@@ -1,23 +1,26 @@
 #ifndef VANITY_CONFIG
 #define VANITY_CONFIG
 
+#include <cuda_runtime.h>
+#include <stdio.h>
+
 static int MAX_ITERATIONS = 100000;
 static int STOP_AFTER_KEYS_FOUND = 100;
 
 // how many times a gpu thread generates a public key in one go
-__device__ int ATTEMPTS_PER_EXECUTION;
+static __device__ int ATTEMPTS_PER_EXECUTION;
 static int attempts_per_exec = 100000;
 
-__device__ const int MAX_PATTERNS = 10;
+static __device__ const int MAX_PATTERNS = 10;
 
 // Arrays for both prefix and suffix patterns
-__device__ char prefixes[MAX_PATTERNS][32];
-__device__ char suffixes[MAX_PATTERNS][32];
+static __device__ char prefixes[MAX_PATTERNS][32];
+static __device__ char suffixes[MAX_PATTERNS][32];
 
 // Counters for number of patterns
-__device__ int prefix_count_device;
-__device__ int suffix_count_device;
+static __device__ int prefix_count_device;
+static __device__ int suffix_count_device;
 
-__device__ __constant__ FILE *output_file;
+static __device__ __constant__ FILE *output_file;
 
 #endif
